b857cee38c54d2aec01dd2fd5368499bac03582d
Xin Tan <mactanxin@gmail.com>
2023-09-20 19:30:43 +0800

    🔧 chore(mini.lua): add support for AI plugin 🔧 chore(mini.lua): configure custom textobjects for AI plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,6 +34,20 @@ return {
           width_nofocus = 25,
         },
       })
+      local ai = require("mini.ai")
+      ai.setup({
+        {
+          n_lines = 500,
+          custom_textobjects = {
+            o = ai.gen_spec.treesitter({
+              a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+              i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+            }, {}),
+            f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+            c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          },
+        },
+      })
     end,
   },
 }
