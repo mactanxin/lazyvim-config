88847def9a3fa2d8ed18cedf2f9081555e4a3bfb
Xin Tan <mactanxin@gmail.com>
2023-09-20 18:54:39 +0800

    🔧 fix(lualine.lua): fix indentation and reorganize code structure ✨ feat(lualine.lua): add event triggers for lualine setup and add wtf module
    🐛 fix(lualine.lua): remove unused 'tablinetabline' refresh option
    ✨ feat(lualine.lua): update lualine configuration to show current filename and status, and remove unnecessary sections

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,104 +1,103 @@
+local function get_attached_clients()
+  local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+  if #buf_clients == 0 then
+    return "LSP Inactive"
+  end
+
+  local buf_ft = vim.bo.filetype
+  local buf_client_names = {}
+
+  -- add client
+  for _, client in pairs(buf_clients) do
+    if client.name ~= "copilot" and client.name ~= "null-ls" then
+      table.insert(buf_client_names, client.name)
+    end
+  end
+
+  -- Generally, you should use either null-ls or nvim-lint + formatter.nvim, not both.
+
+  -- Add sources (from null-ls)
+  -- null-ls registers each source as a separate attached client, so we need to filter for unique names down below.
+  local null_ls_s, null_ls = pcall(require, "null-ls")
+  if null_ls_s then
+    local sources = null_ls.get_sources()
+    for _, source in ipairs(sources) do
+      if source._validated then
+        for ft_name, ft_active in pairs(source.filetypes) do
+          if ft_name == buf_ft and ft_active then
+            table.insert(buf_client_names, source.name)
+          end
+        end
+      end
+    end
+  end
+
+  -- Add linters (from nvim-lint)
+  local lint_s, lint = pcall(require, "lint")
+  if lint_s then
+    for ft_k, ft_v in pairs(lint.linters_by_ft) do
+      if type(ft_v) == "table" then
+        for _, linter in ipairs(ft_v) do
+          if buf_ft == ft_k then
+            table.insert(buf_client_names, linter)
+          end
+        end
+      elseif type(ft_v) == "string" then
+        if buf_ft == ft_k then
+          table.insert(buf_client_names, ft_v)
+        end
+      end
+    end
+  end
+
+  -- Add formatters (from formatter.nvim)
+  local formatter_s, _ = pcall(require, "formatter")
+  if formatter_s then
+    local formatter_util = require("formatter.util")
+    for _, formatter in ipairs(formatter_util.get_available_formatters_for_ft(buf_ft)) do
+      if formatter then
+        table.insert(buf_client_names, formatter)
+      end
+    end
+  end
+
+  -- This needs to be a string only table so we can use concat below
+  local unique_client_names = {}
+  for _, client_name_target in ipairs(buf_client_names) do
+    local is_duplicate = false
+    for _, client_name_compare in ipairs(unique_client_names) do
+      if client_name_target == client_name_compare then
+        is_duplicate = true
+      end
+    end
+    if not is_duplicate then
+      table.insert(unique_client_names, client_name_target)
+    end
+  end
+
+  local client_names_str = table.concat(unique_client_names, ", ")
+  local language_servers = string.format("[%s]", client_names_str)
+
+  return language_servers
+end
+
 return {
-  -- lualine with config
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    dependencies = { { "nvim-tree/nvim-web-devicons", opt = true }, "linrongbin16/lsp-progress.nvim" },
+    event = { "VimEnter", "BufReadPost", "BufNewFile" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      local function get_attached_clients()
-        local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
-        if #buf_clients == 0 then
-          return "LSP Inactive"
-        end
-
-        local buf_ft = vim.bo.filetype
-        local buf_client_names = {}
-
-        -- add client
-        for _, client in pairs(buf_clients) do
-          if client.name ~= "copilot" and client.name ~= "null-ls" then
-            table.insert(buf_client_names, client.name)
-          end
-        end
-
-        -- Generally, you should use either null-ls or nvim-lint + formatter.nvim, not both.
-
-        -- Add sources (from null-ls)
-        -- null-ls registers each source as a separate attached client, so we need to filter for unique names down below.
-        local null_ls_s, null_ls = pcall(require, "null-ls")
-        if null_ls_s then
-          local sources = null_ls.get_sources()
-          for _, source in ipairs(sources) do
-            if source._validated then
-              for ft_name, ft_active in pairs(source.filetypes) do
-                if ft_name == buf_ft and ft_active then
-                  table.insert(buf_client_names, source.name)
-                end
-              end
-            end
-          end
-        end
-
-        -- Add linters (from nvim-lint)
-        local lint_s, lint = pcall(require, "lint")
-        if lint_s then
-          for ft_k, ft_v in pairs(lint.linters_by_ft) do
-            if type(ft_v) == "table" then
-              for _, linter in ipairs(ft_v) do
-                if buf_ft == ft_k then
-                  table.insert(buf_client_names, linter)
-                end
-              end
-            elseif type(ft_v) == "string" then
-              if buf_ft == ft_k then
-                table.insert(buf_client_names, ft_v)
-              end
-            end
-          end
-        end
-
-        -- Add formatters (from formatter.nvim)
-        local formatter_s, _ = pcall(require, "formatter")
-        if formatter_s then
-          local formatter_util = require("formatter.util")
-          for _, formatter in ipairs(formatter_util.get_available_formatters_for_ft(buf_ft)) do
-            if formatter then
-              table.insert(buf_client_names, formatter)
-            end
-          end
-        end
-
-        -- This needs to be a string only table so we can use concat below
-        local unique_client_names = {}
-        for _, client_name_target in ipairs(buf_client_names) do
-          local is_duplicate = false
-          for _, client_name_compare in ipairs(unique_client_names) do
-            if client_name_target == client_name_compare then
-              is_duplicate = true
-            end
-          end
-          if not is_duplicate then
-            table.insert(unique_client_names, client_name_target)
-          end
-        end
-
-        local client_names_str = table.concat(unique_client_names, ", ")
-        local language_servers = string.format("[%s]", client_names_str)
-
-        return language_servers
-      end
       local attached_clients = {
         get_attached_clients,
         color = {
           gui = "bold",
         },
       }
+      local wtf = require("wtf")
       require("lualine").setup({
         options = {
           icons_enabled = true,
           theme = "auto",
-          --[[ theme = chooseRandomTheme(), ]]
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
           disabled_filetypes = {
@@ -110,7 +109,6 @@ return {
           globalstatus = true, -- need set true
           refresh = {
             statusline = 1000,
-            tablinetabline = 1000,
             tabline = 1000,
             winbar = 1000,
           },
@@ -120,11 +118,15 @@ return {
             "mode",
           },
           lualine_b = { "branch", "diff", "diagnostics" },
-          -- show the current filename and session name,
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "filetype" },
-          lualine_y = { "wtf.get_status", "progress" },
-          lualine_z = { attached_clients, "location", require("lsp-progress").progress },
+          -- show the current filename
+          lualine_c = { "filename", wtf.get_status },
+          lualine_x = {
+            attached_clients,
+            "encoding",
+            "filetype",
+          },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
         },
         inactive_sections = {
           lualine_a = {},
@@ -134,14 +136,7 @@ return {
           lualine_y = {},
           lualine_z = {},
         },
-        tabline = {
-          lualine_a = { "buffers" },
-          lualine_b = { "branch" },
-          lualine_c = { "filename" },
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { "tabs" },
-        },
+        tabline = {},
         extensions = { "fzf", "neo-tree", "fugitive" },
       })
     end,
