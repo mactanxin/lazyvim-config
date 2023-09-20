cb050faf183bf58669eadc1f0955d5e9b267d95e
Xin Tan <mactanxin@gmail.com>
2023-09-20 18:54:11 +0800

    nvim-navic, tailwindcss-colors

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -112,4 +112,32 @@ return {
       position = "right",
     },
   },
+  {
+    "SmiteshP/nvim-navic",
+    lazy = true,
+    init = function()
+      vim.g.navic_silence = true
+      require("lazyvim.util").on_attach(function(client, buffer)
+        if client.server_capabilities.documentSymbolProvider then
+          require("nvim-navic").attach(client, buffer)
+        end
+      end)
+    end,
+    opts = function()
+      return {
+        separator = " ",
+        highlight = true,
+        depth_limit = 5,
+        icons = require("lazyvim.config").icons.kinds,
+      }
+    end,
+  },
+  {
+  "themaxmarchuk/tailwindcss-colors.nvim",
+  -- run the setup function after plugin is loaded 
+  config = function ()
+    -- pass config options here (or nothing to use defaults)
+    require("tailwindcss-colors").setup()
+  end
+  },
 }
