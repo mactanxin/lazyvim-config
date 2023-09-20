7c15f9560d8062d12e7ed30a3e1d8cd8bfd69a5d
Xin Tan <mactanxin@gmail.com>
2023-09-20 18:53:26 +0800

    🔧 chore(lsplines.lua): remove unused code and add newline at the end of file

--- lua/plugins/lsplines.lua
+++ lua/plugins/lsplines.lua
@@ -3,8 +3,6 @@ return {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     config = function()
       require("lsp_lines").setup()
-      vim.diagnostic.config({
-      })
     end,
-  }
+  },
 }
