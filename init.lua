9bc8d33941bb9d4fb4fd21d7d70685d852fa25ab
Xin Tan <mactanxin@gmail.com>
2023-11-23 16:34:46 +0800

    🔧 fix(init.lua): add support for configuring transparency and background color in Neovide 🔧 fix(init.lua): set guifont to "Hack Nerd Font:h20" in Neovide ✨ feat(symbol-usage.lua): add symbol-usage plugin configuration to display symbol references, definitions, and implementations

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  -- Helper function for transparency formatting
+  local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  end
+  vim.o.guifont = "Hack Nerd Font:h20"
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+end
