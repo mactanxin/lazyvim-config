c04fddd26edac7713d980836b3b82bf2d9b5c82c
Xin Tan <mactanxin@gmail.com>
2024-01-11 17:58:27 +0800

    cyberdream colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,11 +9,27 @@ return {
       })
     end,
   },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+      })
+      -- vim.cmd("colorscheme cyberdream") -- set the colorscheme
+    end,
+  },
   --
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      -- colorscheme = "tokyonight",
+      colorscheme = "cyberdream",
     },
   },
 }
