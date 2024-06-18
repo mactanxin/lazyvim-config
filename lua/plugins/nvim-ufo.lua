cc9078b0327a58e455e2e27b97d8dca9540e8fbb
Xin Tan <mactanxin@gmail.com>
2024-06-18 14:01:48 +0800

    fix nvim-ufo, add vue-goto-definition

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -8,6 +8,18 @@ return {
     --   return { "treesitter", "indent" }
     -- end,
     open_fold_hl_timeout = 400,
+    close_fold_kinds_for_ft = {
+      description = [[After the buffer is displayed (opened for the first time), close the
+                    folds whose range with `kind` field is included in this option. For now,
+                    'lsp' provider's standardized kinds are 'comment', 'imports' and 'region'.
+                    This option is a table with filetype as key and fold kinds as value. Use a
+                    default value if value of filetype is absent.
+                    Run `UfoInspect` for details if your provider has extended the kinds.]],
+      default = {
+        "imports",
+        "comment",
+      },
+    },
     -- close_fold_kinds = { "imports", "comment" },
     preview = {
       win_config = {
