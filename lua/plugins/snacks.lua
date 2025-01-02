4dbc1d62fe993eb72a572ea29a9d1663a66487e3
Xin Tan <mactanxin@gmail.com>
2025-01-02 10:45:45 +0800

    update neo-tree with snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,6 +19,11 @@ _,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
                                 `'                            '-._|
     ]]
       local opts = {
+        animate = {
+          duration = 20,
+          easing = "linear",
+          fps = 60,
+        },
         dashboard = {
           preset = {
             header = logo,
@@ -43,6 +48,11 @@ _,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
               { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
               { icon = " ", key = "q", desc = "Quit", action = ":qa" },
             },
+            footer = function()
+              local stats = require("lazy").stats()
+              local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+              return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+            end,
           },
           sections = {
             { section = "header" },
@@ -65,6 +75,33 @@ _,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
               padding = { 2, 2 },
             },
             { title = "MRU", padding = 1, pane = 2 },
+          },
+        },
+        lazygit = {
+          configure = true,
+          config = {
+            os = { editPreset = "nvim-remote" },
+            gui = {
+              -- set to an empty string "" to disable icons
+              nerdFontsVersion = "3",
+            },
+          },
+          theme_path = vim.fs.normalize(vim.fn.stdpath("cache") .. "/lazygit-theme.yml"),
+          -- Theme for lazygit
+          theme = {
+            [241] = { fg = "Special" },
+            activeBorderColor = { fg = "MatchParen", bold = true },
+            cherryPickedCommitBgColor = { fg = "Identifier" },
+            cherryPickedCommitFgColor = { fg = "Function" },
+            defaultFgColor = { fg = "Normal" },
+            inactiveBorderColor = { fg = "FloatBorder" },
+            optionsTextColor = { fg = "Function" },
+            searchingActiveBorderColor = { fg = "MatchParen", bold = true },
+            selectedLineBgColor = { bg = "Visual" }, -- set to `default` to have no background colour
+            unstagedChangesColor = { fg = "DiagnosticError" },
+          },
+          win = {
+            style = "lazygit",
           },
         },
       }
